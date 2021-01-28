4124b70f0d3604c42019e45ce65034c4b81299b3
Joe Swantek <joseph.swantek@northwestern.edu>
2021-01-28 16:06:21 -0600

    SAGE-680: log to stdout instead of rsyslog

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,11 @@ FROM ubuntu:20.04
 RUN apt-get update && apt-get install --no-install-recommends -y \
   python3 \
   python3-click \
-  python3-pip \
-  rsyslog
+  python3-pip
 
 # for testing
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt
-
-# for testing
-COPY ROOTFS/etc/rsyslog.d/* /etc/rsyslog.d/
 
 COPY . /workdir
 WORKDIR /workdir
--- ROOTFS/usr/bin/waggle_node_hostname.py
+++ ROOTFS/usr/bin/waggle_node_hostname.py
@@ -2,16 +2,12 @@
 
 import click
 import logging
-import logging.handlers
 import uuid
 import socket
 from pathlib import Path
 
-logger = logging.getLogger("waggle-node-hostname")
-logger.setLevel(logging.DEBUG)
-handler = logging.handlers.SysLogHandler(address="/dev/log")
-handler.setFormatter(logging.Formatter("waggle-node-hostname: %(message)s"))
-logger.addHandler(handler)
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 def_nodeid = "/etc/waggle/node-id"
 
@@ -25,7 +21,7 @@ def set_hostname(nodeid=None):
     Returns:
         none, exception on error
     """
-    logger.info(f"Set the system hostname [node ID: {nodeid}]")
+    logging.info(f"Set the system hostname [node ID: {nodeid}]")
 
     if not nodeid:
         raise Exception("Unable to set hostname, node ID must not be empty")
@@ -35,9 +31,9 @@ def set_hostname(nodeid=None):
     # set the hostname for this and future boots
     try:
         socket.sethostname(nodeid)
-        logger.info(f"Successfuly set the run-time hostname [{nodeid}]")
+        logging.info(f"Successfuly set the run-time hostname [{nodeid}]")
     except:
-        logger.warning(f"Unable to set the run-time hostname [{nodeid}]")
+        logging.warning(f"Unable to set the run-time hostname [{nodeid}]")
         pass
     with open("/etc/hostname", "w") as file:
         file.write(nodeid)
@@ -49,14 +45,14 @@ def set_hostname(nodeid=None):
 )
 def main(nodeid_file):
 
-    logger.info(f"Waggle set hostname [node ID file: {nodeid_file}]")
+    logging.info(f"Waggle set hostname [node ID file: {nodeid_file}]")
 
     nodeid = None
     try:
         with open(nodeid_file, "r") as f:
             nodeid = f.readline().strip()
     except Exception as e:
-        logger.error(f"Unable to read node ID from file [{nodeid_file}]")
+        logging.error(f"Unable to read node ID from file [{nodeid_file}]")
 
     set_hostname(nodeid)
 
